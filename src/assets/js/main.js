3e3ee98b9b23fac69dc1b687448f33654dfaf7e3
Inaro <59608378+inaro8@users.noreply.github.com>
2023-09-21 10:00:53 +0900

    jsのassetsに空白スペースがあったのを修正

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
-import Common from './ assets/common.js';
-import Loading from './ assets/loading.js';
-import Nav from './ assets/nav.js';
-import PageTop from './ assets/pagetop.js';
-import ScrollAnimation from './ assets/scroll_animation.js';
+import Common from './assets/common.js';
+import Loading from './assets/loading.js';
+import Nav from './assets/nav.js';
+import PageTop from './assets/pagetop.js';
+import ScrollAnimation from './assets/scroll_animation.js';
 
 import Home from './scenes/home.js';
 import About from './scenes/about.js';
